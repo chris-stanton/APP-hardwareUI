f28abb1715c32e07ec5ea6669736fa8f2cb0b1a2
Chris Stanton <cstanton0760@yahoo.com>
2017-11-05 21:34:42 -0600

    completed temp buoldout and styling

--- public/scripts/controllers/temp.controller.js
+++ public/scripts/controllers/temp.controller.js
@@ -5,8 +5,19 @@ myApp.controller('TempController',['InitFactory', '$location', 'alertify', funct
   const self = this;
 
   self.message = 'TempController';
-  
 
+  // radio listener
+  self.tempChange = (uistatus) => {
+    if(uistatus <= 69) {
+      self.status = 'Temp is to LOW';
+    } else if(uistatus > 69 && uistatus < 80) {
+      self.status = 'Temp is OPTIMAL';
+    } else if(uistatus >= 80) {
+      self.status = 'Temp is to HIGH';
+    } else {
+      alertify.error("Error finding status");
+    }
+  };
 
 
 
