b27c577af1819535f607ee9e8c9fde7e832e3ebd
Chris Stanton <cstanton0760@yahoo.com>
2017-11-07 20:38:26 -0600

    removed error messagoing logic from temp controller

--- public/scripts/controllers/temp.controller.js
+++ public/scripts/controllers/temp.controller.js
@@ -13,20 +13,10 @@ myApp.controller('TempController',['InitFactory', '$location', 'alertify', funct
   self.tempChange = (uistatus) => {
     if(uistatus <= 69) {
       self.status = 'Temp is to LOW';
-      if(uistatus === 69) {
-        alertify.error("Temp is to LOW");
-      } else {
-        return
-      };
     } else if(uistatus > 69 && uistatus < 80) {
       self.status = 'Temp is OPTIMAL';
     } else if(uistatus >= 80) {
       self.status = 'Temp is to HIGH';
-      if(uistatus === 80) {
-        alertify.error("Temp is to HIGH");
-      } else {
-        return
-      };
     } else {
       alertify.error("Error finding TEMP status");
     };
