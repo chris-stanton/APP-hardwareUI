d7d170a0294d3e467677e34f697292263a812a75
Chris Stanton <cstanton0760@yahoo.com>
2017-11-13 20:16:41 -0600

    adedd server side auth js files and code

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const arduino = require('./server/routes/arduino.js')
+const passport = require('./server/strategies/user_sql.js');
+const session = require('express-session');
 // const pool = require('./server/modules/database-config');
 
 // defining routes
+const index = require('./routes/auth/index');
+const user = require('./routes/auth/user');
+const register = require('./routes/auth/register');
+
 const routes = require('./server/routes/arduino.js');
 
 // serve back static files
@@ -17,11 +22,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // handle index file separately
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '.public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// Passport Session Configuration
+app.use(session({
+   secret: 'secret',
+   key: 'user',
+   resave: 'true',
+   saveUninitialized: false,
+   cookie: {maxage: 60000, secure: false}
+}));
+
+// starts up passport session
+app.use(passport.initialize());
+app.use(passport.session());
+
 // routes
+app.use('/register', register);
+app.use('/user', user);
+
 app.use('/arduino', arduino);
+
+app.use('/*', index); // catch
 
 // port listening on
 app.set('port', process.env.PORT || 5000);
---
+++ database.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(100) NOT NULL unique,
+	password VARCHAR(100) NOT NULL
+);
