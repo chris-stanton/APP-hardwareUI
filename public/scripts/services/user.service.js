a360dfdadb84bd21cfbed46db7aaed06adbecb40
Chris Stanton <cstanton0760@yahoo.com>
2017-11-17 21:44:40 -0600

    adedd logging in user service

--- public/scripts/services/user.service.js
+++ public/scripts/services/user.service.js
@@ -20,7 +20,7 @@ myApp.factory('UserService', function($http, $location, alertify){
         } else {
           // user has no session, bouncing them back to the login page
           $location.path("/login");
-          console.log('getuser error');
+          console.log('error finding user');
         }
       },(response) => {
         $location.path("/login");
