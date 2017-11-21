9606344e5866cba45aa66c18b48833feb5a73582
Chris Stanton <cstanton0760@yahoo.com>
2017-11-21 10:28:12 -0600

    added logic to user service js

--- public/scripts/services/user.service.js
+++ public/scripts/services/user.service.js
@@ -20,11 +20,11 @@ myApp.factory('UserService', function($http, $location, alertify){
         } else {
           // user has no session, bouncing them back to the login page
           $location.path("/login");
-            console.log('error finding user');
+            console.log('Error finding user in DB');
         }
       },(response) => {
         $location.path("/login");
-          console.log('getuser error: ', response);
+          console.log('user error: ', response);
       });
     },
 
