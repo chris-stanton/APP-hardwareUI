65f634fc95dfb08556c79c612c3ae5f0d028384e
Chris Stanton <cstanton0760@yahoo.com>
2017-11-30 22:17:57 -0600

    correct spacing

--- public/scripts/controllers/subs/temp.controller.js
+++ public/scripts/controllers/subs/temp.controller.js
@@ -21,56 +21,62 @@ myApp.controller('TempController',['InitFactory', '$location', 'alertify', funct
       alertify.error("Error finding TEMP status");
       console.log("Error finding TEMP status");
     }
-  }
+  };
 
   // gets footer status on init
   let uistatus = self.uistatus.temp;
   // controller value on init
   self.tempChange(uistatus);
 
-
+  // chart.js config
   let ctx = document.getElementById("tempChart").getContext('2d');
   let myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ["", "", "", "", "", "", "", "", "", ""],
-        datasets: [
-          {
-            label: 'Temp (F)',
-            hidden: false,
-            data: [70, 71, 72, 73, 74, 75, 76, 72, 71, 70],
-            backgroundColor: [
-                '#000000'
-            ],
-            borderColor: [
-                '#ff0000',
-            ],
-            borderWidth: 1,
-            fill: false
-          },
-          {
-            label: 'Humidity (%)',
-            hidden: true,
-            data: [45, 47, 50, 52, 56, 60, 65, 63, 65, 61],
-            backgroundColor: [
-                '#000000'
-            ],
-            borderColor: [
-                '#0012ff'
-            ],
-            borderWidth: 1,
-            fill: false
-          },
-        ]
+      labels: ["", "", "", "", "", "", "", "", "", ""],
+      datasets: [
+        {
+          label: 'Temp (F)',
+          hidden: false,
+          data: [70, 71, 72, 73, 74, 75, 76, 72, 71, 70],
+          backgroundColor: [
+              '#000000'
+          ],
+          borderColor: [
+              '#ff0000',
+          ],
+          borderWidth: 1,
+          fill: false
+        },
+        {
+          label: 'Humidity (%)',
+          hidden: true,
+          data: [45, 47, 50, 52, 56, 60, 65, 63, 65, 61],
+          backgroundColor: [
+              '#000000'
+          ],
+          borderColor: [
+              '#0012ff'
+          ],
+          borderWidth: 1,
+          fill: false
+        },
+      ]
     },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:false
+      legend: {
+                position: 'top',
+                labels: {
+                  fontColor: '#ffffff'
                 }
-            }]
-        }
+              },
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero:false
+              }
+          }]
+      }
     }
   });
 
