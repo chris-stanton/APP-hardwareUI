03db2186f049f5dee072bb5905cc679506c8cfc2
Chris Stanton <cstanton0760@yahoo.com>
2017-12-13 20:53:10 -0600

    added to server side files

--- server/modules/database-config.js
+++ server/modules/database-config.js
@@ -24,7 +24,6 @@ if (process.env.DATABASE_URL) {
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
-
 } else {
   // for use locally
   config = {
